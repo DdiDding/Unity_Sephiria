7cef91b68730fd532c60e1578a16d8878e44a782
yeo myoung Kim <kdjwand@gmail.com>
2026-01-17 02:47:40 +0900

    TileMapInstaller 작성

--- Assets/Script/Content/TileMapInstaller.cs
+++ Assets/Script/Content/TileMapInstaller.cs
@@ -1,25 +1,99 @@
-using Game.Map;
-using GameFramework.Event;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityGameFramework.Runtime;
+using UnityEngine.Tilemaps;
 
 namespace Game.Map
 {
-    // “입력 → 결과”만 만드는 순수 실행 이기때문에 static으로 ㅁ낟므
+    /**
+     * @class TileMapInstaller
+     * @brief MapData와 타일 리소스를 기반으로 실제 타일맵에 배치
+     */
     public class TileMapInstaller
     {
-        public void OnMapLoaded(MapData data)
+        private MapData _mapData;
+        private Action _onInstalled;
+
+        private TileProvider _tileProvider;
+
+        // 타일맵 참조 (외부에서 주입받는 것이 이상적)
+        private Tilemap _groundMap;
+        private Tilemap _upperGroundMap;
+        private Tilemap _wallMap;
+        private Tilemap _cliffMap;
+
+        public TileMapInstaller()
         {
-            //InstallGround(data.ground);
-            //InstallWall(data.wall);
-            //InstallUpper(data.upperGround);
+            _tileProvider = new TileProvider();
         }
 
-        private void InstallGround(string[] data)
+        public void Install(
+            MapData mapData,
+            HashSet<int> neededTileIds,
+            Action onInstalled)
         {
-            List<int> v = new List<int>(capacity: 100);
-            
+            _mapData = mapData;
+            _onInstalled = onInstalled;
+
+            // 타일맵 참조 획득 (예시는 Find, 실제로는 주입 권장)
+            BindTilemaps();
+
+            // 필요한 타일 요청
+            //_tileProvider.LoadTiles(
+            //    neededTileIds,
+            //    OnAllTilesLoaded
+            //);
+        }
+
+        private void BindTilemaps()
+        {
+            _groundMap = GameObject.Find("GroundTilemap").GetComponent<Tilemap>();
+            _upperGroundMap = GameObject.Find("UpperGroundTilemap").GetComponent<Tilemap>();
+            _wallMap = GameObject.Find("WallTilemap").GetComponent<Tilemap>();
+            _cliffMap = GameObject.Find("CliffTilemap").GetComponent<Tilemap>();
+        }
+
+        // ----------------------------------
+        // Tile Loaded Callback
+        // ----------------------------------
+
+        private void OnAllTilesLoaded()
+        {
+            InstallLayer(_mapData.ground, _groundMap);
+            InstallLayer(_mapData.upperGround, _upperGroundMap);
+            InstallLayer(_mapData.wall, _wallMap);
+            InstallLayer(_mapData.cliff, _cliffMap);
+
+            _onInstalled?.Invoke();
+        }
+
+        // ----------------------------------
+        // Tile Placement
+        // ----------------------------------
+
+        private void InstallLayer(int[,] layerData, Tilemap tilemap)
+        {
+            if (layerData == null || tilemap == null)
+                return;
+
+            int height = layerData.GetLength(0);
+            int width = layerData.GetLength(1);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int tileId = layerData[y, x];
+                    if (tileId < 0)
+                        continue;
+
+                    //TileBase tile = _tileProvider.GetTile(tileId);
+                    //if (tile == null)
+                    //    continue;
+
+                    //tilemap.SetTile(new Vector3Int(x, -y, 0), tile);
+                }
+            }
         }
     }
 }
