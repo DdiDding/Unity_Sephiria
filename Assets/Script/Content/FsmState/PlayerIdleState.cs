395c572c96b8de54da7dc257e034349457e8055f
yeo myoung Kim <kdjwand@gmail.com>
2026-01-27 00:52:28 +0900

    Add input handling to PlayerIdleState
    Introduced an InputComponent reference in PlayerIdleState to capture and log movement input during the idle state update.

--- Assets/Script/Content/FsmState/PlayerIdleState.cs
+++ Assets/Script/Content/FsmState/PlayerIdleState.cs
@@ -5,18 +5,21 @@ using UnityGameFramework.Runtime;
 
 public class PlayerIdleState : FsmState<Player>
 {
+    protected InputComponent input;
 
     protected override void OnEnter(IFsm<Player> fsm)
     {
         base.OnEnter(fsm);
         Debug.Log("PlayerIdle Enter!");
         //play animaiton
+        input = GameEntry.GetComponent<InputComponent>();
     }
 
     protected override void OnUpdate(IFsm<Player> fsm, float elapseSeconds, float realElapseSeconds)
     {
         base.OnUpdate(fsm, elapseSeconds, realElapseSeconds);
         Debug.Log("PlayerIdle Update!");
-
+        Vector2 move = input.Move;
+        Debug.Log($"Idle Move Input: {move}, sqrMagnitude: {move.sqrMagnitude}");
     }
 }
