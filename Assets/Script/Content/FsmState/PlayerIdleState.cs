3122b497b25df46d15f1a946efcd27452f940113
yeo myoung Kim <kdjwand@gmail.com>
2026-01-31 11:23:10 +0900

    FSM에서 Idle -> Move 전환 구현
    입력 값 받아 각 상태 간 전환이 가능

--- Assets/Script/Content/FsmState/PlayerIdleState.cs
+++ Assets/Script/Content/FsmState/PlayerIdleState.cs
@@ -7,19 +7,27 @@ public class PlayerIdleState : FsmState<Player>
 {
     protected InputComponent input;
 
+    protected override void OnInit(IFsm<Player> fsm)
+    {
+        base.OnInit(fsm);
+        input = GameEntry.GetComponent<InputComponent>();
+    }
     protected override void OnEnter(IFsm<Player> fsm)
     {
         base.OnEnter(fsm);
-        Debug.Log("PlayerIdle Enter!");
-        //play animaiton
-        input = GameEntry.GetComponent<InputComponent>();
     }
 
     protected override void OnUpdate(IFsm<Player> fsm, float elapseSeconds, float realElapseSeconds)
     {
         base.OnUpdate(fsm, elapseSeconds, realElapseSeconds);
         Debug.Log("PlayerIdle Update!");
+
         Vector2 move = input.Move;
-        Debug.Log($"Idle Move Input: {move}, sqrMagnitude: {move.sqrMagnitude}");
+        if (move.sqrMagnitude > 0f)
+        {
+            ChangeState<PlayerMoveState>(fsm);
+        }
+
+        //Debug.Log($"Idle Move Input: {move}, sqrMagnitude: {move.sqrMagnitude}");
     }
 }
--- Assets/Script/Content/FsmState/PlayerMoveState.cs
+++ Assets/Script/Content/FsmState/PlayerMoveState.cs
@@ -1,18 +1,31 @@
 using GameFramework.Fsm;
 using UnityEngine;
+using UnityGameFramework.Runtime;
 public class PlayerMoveState : FsmState<Player>
 {
+    protected InputComponent input;
+
+    protected override void OnInit(IFsm<Player> fsm)
+    {
+        base.OnInit(fsm);
+    }
 
     protected override void OnEnter(IFsm<Player> fsm)
     {
-        //play animaiton
         base.OnEnter(fsm);
-    }
 
+        input = GameEntry.GetComponent<InputComponent>();
+
+    }
     protected override void OnUpdate(IFsm<Player> fsm, float elapseSeconds, float realElapseSeconds)
     {
         base.OnUpdate(fsm, elapseSeconds, realElapseSeconds);
+        Debug.Log("Player Move Update!");
 
-
+        Vector2 move = input.Move;
+        if (move.sqrMagnitude == 0f)
+        {
+            ChangeState<PlayerIdleState>(fsm);
+        }
     }
 }
